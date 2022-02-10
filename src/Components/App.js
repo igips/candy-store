0a5d819b588df9d4dab98974d07b076c21ee4067
igips <igor.szepietowski@gmail.com>
2022-02-10 19:10:44 +0100

    Create header

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./Nav.js";
+import "../Styles/App.css";
+
+function App() {
+  return (
+    <>
+      <Nav></Nav>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+import "../Styles/Nav.css";
+
+function Nav() {
+    
+
+    return (
+        <nav> 
+            <div class="navDiv">
+                <header><h1>Candy Store</h1></header>
+            </div>
+            <div class="navDiv"></div>
+        </nav>
+    );
+}
+
+export default Nav;
