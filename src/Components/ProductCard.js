ae85d2086c099ef052f4aa95c5d5a052b9b31ed2
igips <igor.szepietowski@gmail.com>
2022-02-13 19:19:23 +0100

    Create product card

---
+++ src/Components/ProductCard.js
@@ -0,0 +1,25 @@
+import "../Styles/ProductCard.css";
+
+
+function ProductCard(props) {
+
+
+
+    return (
+        <div className="productCardContainer">
+            <img className="cardImgDiv" src={props.img} alt="" />
+            <p>{props.name}</p>
+            <p>{props.price}</p>
+            <div className="addToCartDiv">
+                <button>ADD TO CART</button>
+
+            </div>
+
+            
+        </div>
+
+    );
+
+}
+
+export default ProductCard;
