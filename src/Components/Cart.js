73d1bf87a08f0dffa717b9e2fdacef02f17142c4
igips <igor.szepietowski@gmail.com>
2022-02-17 22:06:50 +0100

    Add div for displaying added items

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,10 +1,34 @@
 import "../Styles/Cart.css";
 
-function Cart() {
+function Cart(props) {
 	return (
 		<div id="shoppingCart" className="">
 			<span id="cartText">Your Cart</span>
-            <div id="cartItems"></div>
+            <div id="cartItems">
+                {props.cart.map((item) => {
+                    return (
+                        <div key={item.id + "C"} className="cartItem"> 
+                            <div className="cartImgDiv"><img className="cartImg" src={item.photo} alt="" /> </div>
+                            
+                            <div className="cartItemData">
+                                <span><b>{item.name}</b> </span>
+                                <span> £{item.price}</span>
+                                <div className="cartQuanDiv">
+                                    <button>-</button>
+                                    <input className="quantityCart" min="0" name="quantity" defaultValue="1" type="number" />
+                                     <button  className="add">+</button>
+                                </div>
+                            </div>
+
+
+                        
+                        
+                         </div>
+                    );
+                })}
+
+
+            </div>
 			<div id="buttonDivCart">
                 <span id="total">Total: £0.00</span>
 				<button id="checkoutBut" className="cartButtons">Checkout</button>
