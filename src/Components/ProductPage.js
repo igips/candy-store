8a2d217af1bebf8de4197e13810ce56a7828a2e0
igips <igor.szepietowski@gmail.com>
2022-02-17 22:07:55 +0100

    Add state

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,28 +1,32 @@
 import "../Styles/ProductPage.css";
-import fantaStraw from "../img/fantaStraw.jpg";
+import {useParams} from "react-router";
+import {useState} from "react";
 
 
-function ProductPage() {
+function ProductPage(props) {
 
+    const [loc, setLoc] = useState(useParams());
 
+    const [item, setItem] = useState(props.products.filter(item => item.id === loc.id));
+    
 
     return (
         <div id="productPageMain">
             <div id="productPageInnerMain">
                 <div id="productPageLeft">
-                    <img src={fantaStraw} alt="" />
+                    <img src={item[0].photo} alt="" />
                 </div>
 
                 <div id="productPageRight">
-                    <h1 id="productPageTitle">FANTA STRAWBERRY</h1>
-                    <p id="productPagePrice">£1.70</p>
+                    <h1 id="productPageTitle">{item[0].name.toUpperCase()}</h1>
+                    <p id="productPagePrice">£{item[0].price}</p>
                     <div className="number-input">
                         <button ></button>
                         <input className="quantity" min="0" name="quantity" defaultValue="1" type="number" />
                         <button  className="plus"></button>
                     </div>
-                    <button className="addToCartButtonProductPage">ADD TO CART</button>
-                    <article> ‘Fanta Strawberry’ is a refreshing fruity soda, a sweet strawberry flavoured variation of the classic carbonated soft drink – Fanta.</article>
+                    <button onClick={() => props.addToCart(item[0])} className="addToCartButtonProductPage">ADD TO CART</button>
+                    <article>{item[0].desc}</article>
 
                 </div>
 
