aad69a4bf88e72e8bb4f4529d31cf93e71636d1a
igips <igor.szepietowski@gmail.com>
2022-02-17 22:07:36 +0100

    Add links

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,22 +1,24 @@
 import "../Styles/ProductCard.css";
-
+import { Link } from "react-router-dom";
 
 function ProductCard(props) {
 
+	return (
+		<div className="productCardContainer">
+			<Link to={{pathname: `/product/${props.id}`, state: {props}}}>
+				<img className="cardImgDiv" src={props.img} alt="" />
+			</Link>
 
+			<Link style={{ textDecoration: 'none', color: "#444" }} to={`/product/${props.id}`}>
+				<p className="prodName">{props.name}</p>
+			</Link>
 
-    return (
-        <div className="productCardContainer">
-            <img className="cardImgDiv" src={props.img} alt="" />
-            <p className="prodName">{props.name}</p>
-            <p>{props.price}</p>
-            <div className="addToCartDiv">
-                <button>ADD TO CART</button>
-            </div>            
-        </div>
-
-    );
-
+			<p>£{props.price}</p>
+			<div className="addToCartDiv">
+				<button onClick={() => props.addToCart(props.item)}>ADD TO CART</button>
+			</div>
+		</div>
+	);
 }
 
 export default ProductCard;
