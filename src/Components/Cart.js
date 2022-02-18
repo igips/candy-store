51e3a4d570ffac9951d697d18d771c50d4785fee
igips <igor.szepietowski@gmail.com>
2022-02-18 19:11:50 +0100

    add total

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,18 +1,25 @@
 import "../Styles/Cart.css";
+import {useState} from "react";
+
 
 function Cart(props) {
+
+    let total = 0;
+
+
 	return (
 		<div id="shoppingCart" className="">
 			<span id="cartText">Your Cart</span>
             <div id="cartItems">
                 {props.cart.map((item) => {
+                    total += item.price * item.quan;
                     return (
                         <div key={item.id + "C"} className="cartItem"> 
                             <div className="cartImgDiv"><img className="cartImg" src={item.photo} alt="" /> </div>
                             
                             <div className="cartItemData">
                                 <span><b>{item.name}</b> </span>
-                                <span> £{item.price}</span>
+                                <span> £{(item.price * item.quan).toFixed(2)}</span>
                                 <div className="cartQuanDiv">
                                     <button onClick={() => props.sub(item.id)}>-</button>
                                     <input className="quantityCart" min="0" name="quantity" value={item.quan} type="number" readOnly />
@@ -30,7 +37,7 @@ function Cart(props) {
 
             </div>
 			<div id="buttonDivCart">
-                <span id="total">Total: £0.00</span>
+                <span id="total">Total: £{total.toFixed(2)}</span>
 				<button id="checkoutBut" className="cartButtons">Checkout</button>
 				<button id="closeCartBut" className="cartButtons">Close</button>
 			</div>
