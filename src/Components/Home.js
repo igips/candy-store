69fcf0b448c6f68a093ae82cdf95928d31af45a8
igips <igor.szepietowski@gmail.com>
2022-02-20 00:00:39 +0100

    Add links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,33 +4,47 @@ import smallImg2 from "../img/smallImg2.jpg";
 import smallImg3 from "../img/smallImg3.jpg";
 import smallImg4 from "../img/smallImg4.jpg";
 import Carousel from "./Carousel.js";
+import { Link } from "react-router-dom";
 
 function Home() {
-	
-	
-
+	const a = "A"
 	return (
 		<main>
 			<div id="mainContainer">
-				
-            <Carousel></Carousel>
-                <div id="mainLeft">
+				<Carousel></Carousel>
+				<div id="mainLeft">
 					<div id="grid">
 						<div className="imgDiv">
-							<img src={smallImg1} alt="" />
-                            <span>Candy</span>
+							<Link to={`/store/candy`}>
+								<img src={smallImg1} alt="" />
+							</Link>
+							<Link to={`/store/candy`}>
+								<span>Candy</span>
+							</Link>
 						</div>
 						<div className="imgDiv">
-							<img src={smallImg2} alt="" />
-                            <span>POP TARTS</span>
+							<Link to={`/store/poptarts`}>
+								<img src={smallImg2} alt="" />
+							</Link>
+							<Link to={`/store/poptarts`}>
+								<span>POP TARTS</span>
+							</Link>
 						</div>
 						<div className="imgDiv">
-							<img src={smallImg4} alt="" />
-                            <span>FANTA</span>
+							<Link to={`/store/fanta`}>
+								<img src={smallImg4} alt="" />
+							</Link>
+							<Link to={`/store/fanta`}>
+								<span>FANTA</span>
+							</Link>
 						</div>
 						<div className="imgDiv">
-							<img src={smallImg3} alt="" />
-                            <span>UNDER £1!</span>
+							<Link to={`/store/under`}>
+								<img src={smallImg3} alt="" />
+							</Link>
+							<Link to={`/store/under`}>
+								<span>UNDER £1!</span>
+							</Link>
 						</div>
 					</div>
 				</div>
