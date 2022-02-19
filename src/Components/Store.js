7b106a44b9c078ec39fc03926a007a6cd2d82e06
igips <igor.szepietowski@gmail.com>
2022-02-20 00:01:40 +0100

    Add link

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -1,39 +1,37 @@
 import { useEffect, useState } from "react";
 import "../Styles/Store.css";
 import ProductCard from "./ProductCard.js";
-import fantaStraw from "../img/fantaStraw.jpg";
-
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 function Store(props) {
+	let { cat } = useParams();
 
 	const [sort, setSort] = useState("Alphabetically, A-Z");
-	const [products, setProducts] = useState(props.products);
-
+	const [products, setProducts] = useState(() => {
+		if (!cat) {
+			return props.products;
+		} else {
+			const picked = props.products.filter(
+				(item) => item.brand === cat || item.category === cat || item.catB === cat
+			);
+			return picked;
+		}
+	});
 
 	useEffect(() => {
-			setProducts(() => {
-				let sorted;
+		setProducts(() => {
+			if (!cat) {
+				return props.products;
+			} else {
+				const picked = props.products.filter(
+					(item) => item.brand === cat || item.category === cat || item.catB === cat
+				);
+				return picked;
+			}
+		});
+	}, [cat]);
 
-				if(sort === "Alphabetically, A-Z") {
-					sorted = [...products].sort((a, b) => a.name.localeCompare(b.name));
-					
-				} else if (sort === "Alphabetically, Z-A") {
-					sorted = [...products].sort((a, b) => b.name.localeCompare(a.name));
-					
-				} else if (sort === "Price, low to high") {
-					sorted = [...products].sort((a, b) => a.price - b.price);
-					
-				} else if (sort === "Price, high to low") {
-					sorted = [...products].sort((a, b) => b.price - a.price);
-				}
-
-				return sorted;
-			});
-
-		
-	}, [sort]);
-
-	
 	useEffect(() => {
 		function navDropFunction() {
 			if (!document.getElementById("storeNav").classList.contains("active")) {
@@ -41,17 +39,34 @@ function Store(props) {
 					document.getElementById("storeNav").classList.remove("active2");
 				}
 
-				document.getElementById("storeNav").className += " active";
-			} else {
+				document.getElementById("storeNav").classList.add("active");
+			} else if (document.getElementById("storeNav").classList.contains("active")) {
 				document.getElementById("storeNav").classList.remove("active");
-				document.getElementById("storeNav").className += " active2";
+				document.getElementById("storeNav").classList.add("active2");
 			}
 		}
 
 		document.getElementById("navButton").addEventListener("click", navDropFunction);
+	}, []);
 
-		
-	});
+	useEffect(() => {
+		setProducts(() => {
+			let sorted;
+
+			if (sort === "Alphabetically, A-Z") {
+				sorted = [...products].sort((a, b) => a.name.localeCompare(b.name));
+			} else if (sort === "Alphabetically, Z-A") {
+				sorted = [...products].sort((a, b) => b.name.localeCompare(a.name));
+			} else if (sort === "Price, low to high") {
+				sorted = [...products].sort((a, b) => a.price - b.price);
+			} else if (sort === "Price, high to low") {
+				sorted = [...products].sort((a, b) => b.price - a.price);
+			}
+
+			return sorted;
+		});
+		console.log(cat);
+	}, [sort]);
 
 	const height = {
 		height: "20px",
@@ -76,16 +91,24 @@ function Store(props) {
 					</p>
 					<ul className="catList">
 						<li>
-							<span>Soda</span>
+							<Link className="storeLinks" to={`/store/soda`}>
+								<span>Soda</span>
+							</Link>
 						</li>
 						<li>
-							<span>Pop Tarts</span>
+							<Link className="storeLinks" to={`/store/poptarts`}>
+								<span>Pop Tarts</span>
+							</Link>
 						</li>
 						<li>
-							<span>Candy</span>
+							<Link className="storeLinks" to={`/store/candy`}>
+								<span>Candy</span>
+							</Link>
 						</li>
 						<li className="lastInList">
-							<span>Chocolate</span>
+							<Link className="storeLinks" to={`/store/chocolate`}>
+								<span>Chocolate</span>
+							</Link>
 						</li>
 					</ul>
 
@@ -94,22 +117,40 @@ function Store(props) {
 					</p>
 					<ul id="secondUl" className="catList">
 						<li>
-							<span>Fanta</span>
+							<Link className="storeLinks" to={`/store/fanta`}>
+								<span>Fanta</span>
+							</Link>
 						</li>
 						<li>
-							<span>Hershey's</span>
+							<Link className="storeLinks" to={`/store/hershey's`}>
+								<span>Hershey's</span>
+							</Link>
 						</li>
 						<li>
-							<span>Reese's</span>
+							<Link className="storeLinks" to={`/store/reese's`}>
+								<span>Reese's</span>
+							</Link>
 						</li>
 						<li>
-							<span>Sour Patch</span>
+							<Link className="storeLinks" to={`/store/skittles`}>
+								<span>Skittles</span>
+							</Link>
+						</li>
+
+						<li>
+							<Link className="storeLinks" to={`/store/kellogg's`}>
+								<span>Kellogg's</span>
+							</Link>
 						</li>
 						<li>
-							<span>Skittles</span>
+							<Link className="storeLinks" to={`/store/SourPatch`}>
+								<span>Sour Patch</span>
+							</Link>
 						</li>
 						<li className="lastInList">
-							<span>Peace Tea</span>
+							<Link className="storeLinks" to={`/store/peacetea`}>
+								<span>Peace Tea</span>
+							</Link>
 						</li>
 					</ul>
 				</div>
@@ -126,15 +167,21 @@ function Store(props) {
 							</select>
 						</div>
 					</div>
-                    <div id="productList">
+					<div id="productList">
 						{products.map((item) => {
 							return (
-								<ProductCard item={item} addToCart={props.addToCart} id={item.id} key={item.id} price={item.price} name={item.name} img={item.photo}></ProductCard>
+								<ProductCard
+									item={item}
+									addToCart={props.addToCart}
+									id={item.id}
+									key={item.id}
+									price={item.price}
+									name={item.name}
+									img={item.photo}
+								></ProductCard>
 							);
 						})}
-                       
-
-                    </div>
+					</div>
 				</div>
 			</div>
 		</div>
