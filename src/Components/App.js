b9ac500be5e7efb30aa010d11c53efff0f781f2b
igips <igor.szepietowski@gmail.com>
2022-02-19 16:16:57 +0100

    Add brand and category to products

--- src/Components/App.js
+++ src/Components/App.js
@@ -109,6 +109,8 @@ function App() {
 			photo: fantaStraw,
 			price: "1.70",
 			desc: "‘Fanta Strawberry’ is a refreshing fruity soda, a sweet strawberry flavoured variation of the classic carbonated soft drink – Fanta.",
+      brand: "fanta",
+      category: "soda",
 		},
 		{
 			id: "sourP",
@@ -116,6 +118,8 @@ function App() {
 			photo: sourPatch,
 			price: "1.95",
 			desc: "Give your tongue a real tangy-tingling sensation with this 3.5oz theater box of Sour Patch Kids! These delightfully fruity gummies are coated in a sour sugar that will fizz on your tongue and make your mouth-pucker, before giving way to a soft and sweet chew beneath. Each one of these delicious candies is bursting super sour, tongue tingling fruity flavours! Simply mouth-watering!",
+      brand: "Sour Patch",
+      category: "candy"
 		},
 		{
 			id: "popTart",
@@ -123,6 +127,8 @@ function App() {
 			photo: popTarts,
 			price: "1.39",
 			desc: "One of America's favourite snacks, loaded with flavourful fillings and usually frosted with an icing to match. They're great straight out of the box, but even better toasted to warm and gooey perfection in a toaster or microwave oven. Each pastry consists of a delicious cherry-flavoured filling, pink frosting, and sparkly red sprinkles: it's cherry heaven!",
+      brand: "kellogg's",
+      category: "pop tarts",
 		},
 		{
 			id: "hersB",
@@ -130,6 +136,8 @@ function App() {
 			photo: hersB,
 			price: "0.70",
 			desc: "If you’re looking to add a little hippity-hoppity happiness to your Easter baskets this spring, look no further than HERSHEY'S Milk Chocolate Bunny.",
+      brand: "hershey's",
+      category: "chocolate",
 		},
 		{
 			id: "skittles",
@@ -137,6 +145,8 @@ function App() {
 			photo: skittles,
 			price: "2.20",
 			desc: "The Skittles range of flavours gets ever bigger with the addition of Skittles Tropical! Each bag contains Kiwi Lime, Banana Berry, Strawberry Starfruit, Mango Tangelo and Pineapple Passionfruit candies and trust us when we tell you they're AMAZING! Grab a bag and taste the exotic tropical side of the rainbow! Ultimate Summer snack.",
+      brand: "skittles",
+      category: "candy"
 		},
 		{
 			id: "reeses",
@@ -144,6 +154,8 @@ function App() {
 			photo: reeses,
 			price: "1.39",
 			desc: "A wise man once said the best things in life come in threes, and he was right! That's why there's 3 of Reese's finest peanut butter cups in this pack. All that delicious creamy, crumbly Reese's PNB has been lovely encased in a milk chocolate cup to celebrate this confectionery combination.",
+      brand: "reese's",
+      category: "chocolate",
 		},
 		{
 			id: "peace",
@@ -151,6 +163,8 @@ function App() {
 			photo: peace,
 			price: "1.69",
 			desc: "Green Tea Flavour Peace Iced Tea (often labeled Peace Tea) is a brand of iced tea beverages produced by the Coca-Cola Company. There's peace, happiness, and fresh iced tea flavor in every can of Peace Tea. Take a sip of something refreshingly delicious and enjoy Peace Tea today!- 695ml",
+      brand: "peace tea",
+      category: "soda",
 		},
 	]);
 
