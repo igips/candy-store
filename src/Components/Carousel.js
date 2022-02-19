4d8a2847c29af367c18be53724522c8461686cf6
igips <igor.szepietowski@gmail.com>
2022-02-20 00:00:27 +0100

    Add links

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,29 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import background from "../img/back.jpg";
 import back2 from "../img/back2.jpg";
 import back3 from "../img/back3.jpg";
 import back4 from "../img/back4.jpg";
+import { Link } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
 
 function MyCarousel() {
+	
+
 	return (
 		<div id="mainRight">
-			<div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-				<div className="carousel-inner">
-					<div className="carousel-item active">
-						<img className="d-block w-100" src={background} alt="First slide" />
-					</div>
-					<div className="carousel-item">
-						<img className="d-block w-100" src={back2} alt="Second slide" />
-					</div>
-					<div className="carousel-item">
-						<img className="d-block w-100" src={back3} alt="Third slide" />
-					</div>
-                    <div className="carousel-item">
-						<img className="d-block w-100" src={back4} alt="Fourth slide" />
-					</div>
-				</div>
-			</div>
+			<Carousel>
+				<Link to={`/store/candy`} className="carousel-item" >
+						<img className="d-block w-100" src={background} alt="First slide" /> 
+				</Link >
+				<Link to={`/store/chocolate`} className="carousel-item" >
+					<img className="d-block w-100" src={back2} alt="Second slide" /> 
+				</Link>
+				<Link to={`/store/peacetea`} className="carousel-item" >
+					<img className="d-block w-100" src={back3} alt="Third slide" />
+				</Link>
+				<Link to={`/store/hershey's`} className="carousel-item" >
+					<img className="d-block w-100" src={back4} alt="Fourth slide" /> 
+				</Link>
+			</Carousel>
 		</div>
 	);
 }
